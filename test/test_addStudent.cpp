c800953d018f66ed13c78da19a84a9f281a628c4
Haider Bassem <haideralatbe@gmail.com>
2025-10-31 17:18:46 +0300

    Add test executable and implement tests for PeopleDataHandler
    - Created a new test executable AfqTests with multiple source files.
    - Implemented a test for adding a person in PeopleDataHandler.
    - Added a main function for running GoogleTest with Qt integration.

--- test/test_addStudent.cpp
+++ test/test_addStudent.cpp
@@ -1,26 +1,25 @@
-// #include <gtest/gtest.h>
-// #include "data/StudentDataHandler.h"
-// #include "DataModel/Student.h"
-// #include "data/PeopleDataHandler.h"
-// #include "DataModel/Person.h"
-// #include <QDate>
+#include <gtest/gtest.h>
+#include "DataModel/Student.h"
+#include "data/PeopleDataHandler.h"
+#include "DataModel/Person.h"
+#include <QDate>
 
-// using namespace DataAccess;
-// using namespace DataModel;
-// Person p;
-// TEST(PeopleDataHandlerTest, AddPerson_valid)
-// {
-//     p.first_name = "Haider";
-//     p.second_name = "Bassem";
-//     p.third_name = "Nassif";
-//     p.fourth_name = "Jassim";
-//     p.date_of_birth = QDate(2004,9,20);
-//     p.gender = Gender::Male;
+using namespace DataAccess;
+using namespace DataModel;
+Person p;
+TEST(PeopleDataHandlerTest, AddPerson_valid)
+{
+    p.first_name = "Haider";
+    p.second_name = "Bassem";
+    p.third_name = "Nassif";
+    p.fourth_name = "Jassim";
+    p.date_of_birth = QDate(2004,9,20);
+    p.gender = Gender::Male;
 
-//     auto result = PeopleDataHandler::addPerson(p);
-//     ASSERT_TRUE(result.has_value());
-//     EXPECT_EQ(result.value(),1);
-// }
+    auto result = PeopleDataHandler::addPerson(p);
+    ASSERT_TRUE(result.has_value());
+    EXPECT_EQ(result.value(),1);
+}
 
 // // TEST(StudentDataHandlerTest, AddStudent_Valid) {
 // //     Student s;
